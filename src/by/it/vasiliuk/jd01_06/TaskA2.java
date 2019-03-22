4ee645c3bdef331ce6a74d17005ca13416b06194
Mercurialcool <Vadim.minsk61@gmail.com>
2019-03-22 12:29:58 +0300

    jd01_06 TaskA1 done

--- src/by/it/vasiliuk/jd01_06/TaskA2.java
+++ src/by/it/vasiliuk/jd01_06/TaskA2.java
@@ -1,5 +1,31 @@
 package by.it.vasiliuk.jd01_06;
 
-public class TaskA2 {
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class TaskA2 {
+    private static String[] words = new String[0];
+    private static int[] counters = new int[0];
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]{4,}");
+        Matcher matcher = pattern.matcher(Poem.text);
+        while (matcher.find()){
+            String word = matcher.group();
+            process(word);
+        }
+    }
+    private static void process(String word){
+        for(int i = 0;i < words.length; i++){
+            if (words[i].equals(word)){
+                counters[i]++;
+                return;
+            }
+        }
+        words=Arrays.copyOf(words,counters.length+1);
+        words[words.length-1]=word;
+
+        counters=Arrays.copyOf(counters,counters.length+1);
+        counters[words.length-1]=1;
+    }
 }

b1286f8e4c76d1a9c50990183e7ce387fbca7a57
Mercurialcool <Vadim.minsk61@gmail.com>
2019-03-25 08:37:27 +0300

    jd01_02 finished C jd01_05 finished C jd01_06 finished A B

--- src/by/it/vasiliuk/jd01_06/TaskA2.java
+++ src/by/it/vasiliuk/jd01_06/TaskA2.java
@@ -3,29 +3,35 @@ package by.it.vasiliuk.jd01_06;
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 public class TaskA2 {
-    private static String[] words = new String[0];
+    private  static String[] words = new String[0];
     private static int[] counters = new int[0];
+
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]{4,}");
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
         Matcher matcher = pattern.matcher(Poem.text);
-        while (matcher.find()){
-            String word = matcher.group();
+        while(matcher.find()){
+            String word  = matcher.group();
             process(word);
         }
+        for (int i = 0; i <words.length ; i++) {
+            System.out.printf("%s=%d%n",words[i],counters[i]);
+
+        }
     }
-    private static void process(String word){
-        for(int i = 0;i < words.length; i++){
-            if (words[i].equals(word)){
+
+    private static void process(String word) {
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word)) {
                 counters[i]++;
                 return;
             }
         }
-        words=Arrays.copyOf(words,counters.length+1);
-        words[words.length-1]=word;
+        words = Arrays.copyOf(words, words.length + 1);
+        words[words.length - 1]=word;
 
-        counters=Arrays.copyOf(counters,counters.length+1);
-        counters[words.length-1]=1;
+        counters = Arrays.copyOf(counters, counters.length + 1);
+        counters[words.length - 1]=1;
+
     }
 }

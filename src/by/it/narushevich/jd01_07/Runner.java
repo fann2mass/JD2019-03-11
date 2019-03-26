cbd670b570950577949338dae4e0abfc217b40cb
Narushevich Marianna <marianna.naru@gmail.com>
2019-03-26 14:33:33 +0300

    jd01_07 TaskC

--- src/by/it/narushevich/jd01_07/Runner.java
+++ src/by/it/narushevich/jd01_07/Runner.java
@@ -1,7 +1,5 @@
 package by.it.narushevich.jd01_07;
 
-import java.util.Arrays;
-
 public class Runner {
 
     public static void main(String[] args) {
@@ -15,11 +13,19 @@ public class Runner {
 
         double[] array = {1.5, 2.6, 8.1, 6.7};
         Vector v4 = new Vector(array);
-        Var v5 = new Vector(v4);
+        Var v5 = v4;
         Var v6 = new Vector("{1,2,4}");
 
         System.out.println(v4);
         System.out.println(v5);
         System.out.println(v6);
+
+        double[][] matrix = {{1,2,3},{4,5,6}};
+        Matrix v7 = new Matrix(matrix);
+        System.out.println(v7);
+        Var v8 = new Matrix(v7);
+        System.out.println(v8);
+        Matrix v9 = new Matrix("{{1,2},{3,4}}");
+        System.out.println(v9);
     }
 }

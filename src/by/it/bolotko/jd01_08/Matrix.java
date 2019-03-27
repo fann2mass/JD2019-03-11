71effa9b0bbff6e2c06899f2511d34e55aeedc1f
Konstantsin Bolotko <pro100kosti@gmail.com>
2019-03-27 14:42:53 +0300

    jd01_01 (all) jd01_02 (A,B) jd01_03 (all) jd01_04 (A,B) jd01_05 (all) jd01_06(A,B) jd01_07(A,B) jd01_08(A)

--- src/by/it/bolotko/jd01_08/Matrix.java
+++ src/by/it/bolotko/jd01_08/Matrix.java
@@ -1,4 +1,4 @@
-package by.it.bolotko.jd01_07;
+package by.it.bolotko.jd01_08;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -6,6 +6,30 @@ import java.util.regex.Pattern;
 class Matrix extends Var {
 
     private double[][] matrix;
+
+    public double[][] getMatrix() {
+        return matrix;
+    }
+
+    @Override
+    public Var add(Var other) {
+        return super.add(other);
+    }
+
+    @Override
+    public Var sub(Var other) {
+        return super.sub(other);
+    }
+
+    @Override
+    public Var mul(Var other) {
+        return super.mul(other);
+    }
+
+    @Override
+    public Var div(Var other) {
+        return super.div(other);
+    }
 
     Matrix(double[][] matrix) {
         this.matrix = matrix;
---
+++ src/by/it/bolotko/jd01_08/Vector.java
@@ -0,0 +1,64 @@
+package by.it.bolotko.jd01_08;
+
+import java.util.Arrays;
+
+class Vector extends Var {
+
+    private double[] value;
+
+    public double[] getValue() {
+        return value;
+    }
+
+    @Override
+    public Var add(Var other) {
+        return super.add(other);
+    }
+
+    @Override
+    public Var sub(Var other) {
+        return super.sub(other);
+    }
+
+    @Override
+    public Var mul(Var other) {
+        return super.mul(other);
+    }
+
+    @Override
+    public Var div(Var other) {
+        return super.div(other);
+    }
+
+    Vector(double[] value){
+
+        this.value= Arrays.copyOf(value,value.length);
+    }
+
+    Vector(String str) {
+        StringBuilder sb=new StringBuilder(str);
+        sb.deleteCharAt(0).deleteCharAt(sb.length()-1);
+        String result=sb.toString();
+        String[] strArr = result.split("[^\\d.]+");
+        double[] value = new double[strArr.length];
+        for (int i = 0; i < strArr.length; i++) {
+            value[i] = Double.parseDouble(strArr[i]);
+        }
+        this.value=value;
+    }
+
+    Vector(Vector vector){this.value=vector.value;}
+
+
+    @Override
+    public String toString() {
+        StringBuilder res=new StringBuilder("{");
+        String delimiter="";
+        for (double element : value) {
+            res.append(delimiter).append(element);
+            delimiter = ", ";
+        }
+        res.append("}");
+        return res.toString();
+    }
+}

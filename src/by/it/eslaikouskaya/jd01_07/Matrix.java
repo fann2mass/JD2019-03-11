9bd386dc36b5cc46d9a16459f07877f16697da73
yourGitHubusername <Arol_33@mail.ru  git config --global user.name Katyaaaa>
2019-03-28 20:17:56 +0300

    8. A B C

--- src/by/it/eslaikouskaya/jd01_07/Matrix.java
+++ src/by/it/eslaikouskaya/jd01_07/Matrix.java
@@ -1,5 +1,8 @@
 package by.it.eslaikouskaya.jd01_07;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 class Matrix extends Var{
 
 	private double[][] value;
@@ -10,6 +13,22 @@ class Matrix extends Var{
 
 	Matrix(Matrix matrix){
 		this.value=matrix.value;
+	}
+
+
+	Matrix(String strMatrix) {
+		StringBuilder sb = new StringBuilder(strMatrix);
+		sb.deleteCharAt(0).deleteCharAt(0).deleteCharAt(sb.length() - 1).deleteCharAt(sb.length() - 1);
+		String string = sb.toString();
+		String[] str = string.split("[{]");
+		double[][] matrix = new double[str.length][str.length];
+		for (int i = 0; i < str.length; i++) {
+			for (int j = 0; j < str.length; j++) {
+				String[] row = str[i].split("[^\\d.]+");
+				matrix[i][j] = Double.parseDouble(row[j]);
+			}
+			this.value = matrix;
+		}
 	}
 
 
@@ -24,7 +43,7 @@ class Matrix extends Var{
 				delimiter = ", ";
 			}
 			if (i!=value.length-1)
-			{sb.append("},{");}
+			sb.append("},{");
 			delimiter = "";
 		}
 		sb.append("}}");

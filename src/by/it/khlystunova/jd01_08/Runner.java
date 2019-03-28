b8c2112d79abff846fdc621b8c545fd51081e6c6
Khlystunova-Liza <lisa.epee@gmail.com>
2019-03-28 21:24:31 +0300

    jd01_08 done

--- src/by/it/khlystunova/jd01_08/Runner.java
+++ src/by/it/khlystunova/jd01_08/Runner.java
@@ -1,35 +1,32 @@
 package by.it.khlystunova.jd01_08;
 
 public class Runner {
+    private static void print(Var var) {
+        if (var!=null)
+            System.out.println(var);
+    }
+
     public static void main(String[] args) {
-        //Task A
-        Var var1 = new Scalar(3.1415);//конструктор из вещесвенного числа
-        System.out.println(var1);
-        Scalar var2 = (Scalar)var1;
-        Var var3 = new Scalar(var2);//конструктор из переменной Scalar
-        System.out.println(var3);
-        Var var4 = new Scalar("3.1415");
-        System.out.println(var4);
-        //Task B
-        Var v1 = new Vector(new double[]{1.0, 2.0, 4.0});
-        System.out.println(v1);
-
-        Vector v2 = (Vector)v1;
-        Var v3 = new Vector(v2);//конструктор из переменной Vector
-        System.out.println(v3);
-
-        Var v4 = new Vector("{1.0, 2.0, 4.0}");
-        System.out.println(v4);
-        //TaskB
-        Var array1 = new Matrix(new double[][]{{1.0, 2.0 },{ 3.0, 4.0 }});
-        System.out.println(array1);
-
-        Matrix array2 = (Matrix)array1;
-        Var array3 = new Matrix(array2);
-        System.out.println(array3);
-
-        Var array4 = new Matrix("{{1, 2}, {3, 4}}");
-        System.out.println(array4);
+        Var s = new Scalar(3.0);
+        Var v = new Vector(new double[]{1, 2, 3});
+        Var m = new Matrix("{{1,2,3},{4,5,6},{7,8,9}}");
+        /* Уровень сложности A (калькулятор) */
+        print(s.add(s)); //выведет в консоль 6.0
+        print(s.sub(s)); //выведет в консоль 0.0
+        print(s.mul(s)); //выведет в консоль 9.0
+        print(s.div(s)); //выведет в консоль 1.0
+        /* Уровень сложности B (векторные операции) закомментируйте, если не реализовали */
+        print(v.add(v)); //выведет в консоль {2.0, 4.0, 6.0}
+        print(v.sub(v)); //выведет в консоль {0.0, 0.0, 0.0}
+        print(v.mul(v)); //выведет в консоль 14.0
+        print(v.div(v)); //сообщит о невозможности операции
+        /* Уровень сложности C (матричные операции и умножение на вектор)
+        закомментируйте, если не реализовали */
+        print(m.add(m)); //{{2.0, 4.0, 6.0}, {8.0, 10.0, 12.0}, {14.0, 16.0, 18.0}}
+        print(m.sub(m)); //{{0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}, {0.0, 0.0, 0.0}}
+        print(m.mul(m)); //{{30.0, 36.0, 42.0}, {66.0, 81.0, 96.0}, {102.0, 126.0, 150.0}}
+        print(m.mul(v)); //{14.0, 32.0, 50.0}
+        print(m.mul(s));
     }
 
 }
--- src/by/it/khlystunova/jd01_08/Vector.java
+++ src/by/it/khlystunova/jd01_08/Vector.java
@@ -1,5 +1,6 @@
 package by.it.khlystunova.jd01_08;
 
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,8 +8,11 @@ public class Vector extends Var {
 
     private double[] vector;
 
+    public double[] getVector() {
+        return vector;
+    }
     Vector(double[] value) {
-        this.vector = value;
+        this.vector = Arrays.copyOf(value, value.length);
     }
 
     Vector(Vector vector) {
@@ -46,5 +50,77 @@ public class Vector extends Var {
             else result.append(", ");
         }
         return result.toString();
+    }
+
+    @Override
+    public Var add(Var other) {
+        Vector result = new Vector(this.vector);//копируем вектор, который вызвал этот метод(this.vector) в вектор результата.
+         if(other instanceof Vector){
+             if(((Vector) other).vector.length == result.vector.length){
+                 for (int i = 0; i < result.vector.length; i++) {
+                     result.vector[i] =result.vector[i]+((Vector) other).vector[i];
+                 }
+             }
+           return result;
+         }else if(other instanceof Scalar){
+             for (int i = 0; i < result.vector.length; i++) {
+                result.vector[i] = result.vector[i] + ((Scalar)other).getValue();
+             }
+             return result;
+         }
+            return super.add(other);
+    }
+
+    @Override
+    public Var sub(Var other) {
+        Vector result = new Vector(this.vector);//копируем вектор, который вызвал этот метод(this.vector) в вектор результата.
+        if(other instanceof Vector){
+            //если вектор, который вызвал этот метод и вектор other одинаковой длины то вычитаем
+            if(((Vector) other).vector.length == result.vector.length){
+                for (int i = 0; i < result.vector.length; i++) {
+                    result.vector[i] =result.vector[i]-((Vector) other).vector[i];
+                }
+            }
+            return result;
+        }else if(other instanceof Scalar){
+            for (int i = 0; i < result.vector.length; i++) {
+                result.vector[i] = result.vector[i] - ((Scalar)other).getValue();
+            }
+            return result;
+        }
+        return super.sub(other);
+    }
+
+    @Override
+    public Var mul(Var other) {
+        Vector result = new Vector(this.vector);//копируем вектор, который вызвал этот метод(this.vector) в вектор результата.
+        if(other instanceof Vector){
+            //если вектор, который вызвал этот метод и вектор other одинаковой длины то вычитаем
+            double value = 0;
+            if(((Vector) other).vector.length == result.vector.length){
+                for (int i = 0; i < result.vector.length; i++) {
+                    value +=result.vector[i]*((Vector) other).vector[i];
+                }
+            }
+            return new Scalar(value);
+        }else if(other instanceof Scalar){
+            for (int i = 0; i < result.vector.length; i++) {
+                result.vector[i] = result.vector[i] * ((Scalar)other).getValue();
+            }
+            return result;
+        }
+        return super.mul(other);
+    }
+
+    @Override
+    public Var div(Var other) {
+        Vector result = new Vector(this.vector);//копируем вектор, который вызвал этот метод(this.vector) в вектор результата.
+        if(other instanceof Scalar){
+            for (int i = 0; i < result.vector.length; i++) {
+                result.vector[i] = result.vector[i] / ((Scalar)other).getValue();
+            }
+            return result;
+        }
+        return super.div(other);
     }
 }

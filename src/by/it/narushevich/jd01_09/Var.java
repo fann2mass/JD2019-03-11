a4591669bb01bfac4efa31c98c6826bd99be43ba
Narushevich Marianna <marianna.naru@gmail.com>
2019-03-29 16:24:41 +0300

    jd01_09 TaskA, B, C

--- src/by/it/narushevich/jd01_09/Var.java
+++ src/by/it/narushevich/jd01_09/Var.java
@@ -2,7 +2,7 @@ package by.it.narushevich.jd01_09;
 
 abstract class Var implements Operation {
 
-    static Var createVar(String operand){
+    static Var createVar(String operand) {
         operand = operand.trim();
         if (operand.matches(Patterns.SCALAR))
             return new Scalar(operand);
@@ -10,32 +10,30 @@ abstract class Var implements Operation {
             return new Vector(operand);
         if (operand.matches(Patterns.MATRIX))
             return new Matrix(operand);
-
-
         return null;
     }
 
     @Override
     public Var add(Var other) {
-        System.out.println("Сложение " + this + " + " + other + " невозможно!");
+        System.out.printf("Операция сложения %s+%s невозможна!\n",this,other);
         return null;
     }
 
     @Override
     public Var sub(Var other) {
-        System.out.println("Вычитание " + this + " - " + other + " невозможно!");
+        System.out.printf("Операция вычитания %s+%s невозможна!\n",this,other);
         return null;
     }
 
     @Override
     public Var mul(Var other) {
-        System.out.println("Умножение " + this + " * " + other + " невозможно!");
+        System.out.printf("Операция умножения %s+%s невозможна!\n",this,other);
         return null;
     }
 
     @Override
     public Var div(Var other) {
-        System.out.println("Деление " + this + " / " + other + " невозможно!");
+        System.out.printf("Операция деления %s+%s невозможна!\n",this,other);
         return null;
     }
 

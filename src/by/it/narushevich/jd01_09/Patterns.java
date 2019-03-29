ee551e0337f2e23cbd5f3c90c458479151f6eca0
Narushevich Marianna <marianna.naru@gmail.com>
2019-03-29 16:06:30 +0300

    jd01_09

--- src/by/it/narushevich/jd01_09/Patterns.java
+++ src/by/it/narushevich/jd01_09/Patterns.java
@@ -1,12 +1,8 @@
 package by.it.narushevich.jd01_09;
 
 class Patterns {
-
-    private Patterns() {
-
-    }
-
     static final String OPERATION="[-+*/=]";
     static final String SCALAR="(-?)\\d+(\\.\\d+)?";
-    static final String VECTOR="\\{";
+    static final String VECTOR="\\{(-?\\d+(\\.\\d+)?,\\s?)+-?\\d+(\\.\\d+)?\\}";
+    static final String MATRIX="\\{((\\{(-?\\d+(\\.\\d+)?,\\s?)+-?\\d+(\\.\\d+)?\\}),?)+}";
 }

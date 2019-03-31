6fcb05d50d7e336d0a4f100ce7e6984e4b7559c3
User <ifann2mass@gmail.com>
2019-03-31 19:41:21 +0300

    TaskA.01_09 Classwork

---
+++ src/by/it/zalesky/jd01_09/Vector.java
@@ -0,0 +1,55 @@
+package by.it.zalesky.jd01_09;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Vector extends Var {
+
+    private double[] value;
+
+    Vector(double[] value) {
+        this.value = value;
+    }
+
+
+    Vector(String vector) {
+
+        Pattern pattern = Pattern.compile("[^{}, ]+");
+        Matcher matcher = pattern.matcher(vector);
+        int vec = 0;
+        while (matcher.find()) {
+            vec++;
+        }
+        matcher.reset();
+        double[] array = new double[vec];
+        int i = 0;
+        while (matcher.find()){
+            String str = matcher.group();
+            array[i] = Double.parseDouble(str);
+            i++;
+        }
+        this.value = array;
+    }
+
+
+    private double value(int i) {
+        return 0;
+    }
+
+    Vector(Vector vector) {
+        this.value = vector.value;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("{");
+        String delimeter = "";
+        for (double element : value) {
+            sb.append(delimeter).append(element);
+            delimeter = ", ";
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+
+}

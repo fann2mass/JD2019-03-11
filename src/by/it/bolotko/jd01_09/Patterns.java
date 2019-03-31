f5a58908d12df0549f4f982dfa9cf3b5f640765b
Konstantsin Bolotko <pro100kosti@gmail.com>
2019-03-31 18:43:43 +0300

    jd01_01 (all) jd01_02 (A,B) jd01_03 (all) jd01_04 (A,B) jd01_05 (all) jd01_06(A,B) jd01_07(all) jd01_08(all) jd01_09(all)

---
+++ src/by/it/bolotko/jd01_09/Patterns.java
@@ -0,0 +1,9 @@
+package by.it.bolotko.jd01_09;
+
+class Patterns {
+
+    static final String OPERATION = "[-+/*]";
+    static final String SCALAR = "-?[0-9]+\\.?[0-9]*";
+    static final String VECTOR = "\\{((-?[0-9]+\\.?[0-9]*),?)+}";
+    static final String MATRIX = "\\{(\\{((-?[0-9]+\\.?[0-9]*),?)+},?)+}";
+}

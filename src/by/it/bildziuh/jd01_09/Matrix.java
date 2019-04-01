373ac978d5fa04b585a75bb9ec7555bbb7deed4f
Bildziuh <crazydaem@gmail.com>
2019-04-01 21:41:28 +0300

    jd01_09 - matrix reforged

--- src/by/it/bildziuh/jd01_09/Matrix.java
+++ src/by/it/bildziuh/jd01_09/Matrix.java
@@ -1,5 +1,8 @@
 package by.it.bildziuh.jd01_09;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Matrix extends Var {
 
     private double[][] value;
@@ -22,21 +25,33 @@ public class Matrix extends Var {
 
     Matrix(String strMatrix) {
 
-        StringBuilder sb = new StringBuilder(strMatrix);
-        sb.deleteCharAt(0).deleteCharAt(sb.length() - 1);
-        sb.deleteCharAt(0).deleteCharAt(sb.length() - 1);
-        String string = sb.toString();
-        String[] splitedString = string.split("[{]");
-        double[][] matrix = new double[splitedString.length][2];
-        for (int i = 0; i < splitedString.length; i++) {
-            for (int j = 0; j < 2; j++) {
-                String[] row = splitedString[i].split("[^\\d.]+");
+        Pattern patternRows = Pattern.compile("((-?[\\d]+\\.?[\\d]*)\\s?,?\\s?)+");
+        Pattern patternNumbers = Pattern.compile("[\\d]+\\.?+\\d*");
+        Matcher matcherRows = patternRows.matcher(strMatrix);
+        Matcher matcherNumbers = patternNumbers.matcher(strMatrix);
+        int rows = 0;
+        int numbers = 0;
+        while (matcherRows.find()) {
+            rows++;
+        }
+        while (matcherNumbers.find()) {
+            numbers++;
+        }
+        int cols = numbers / rows;
+
+        String[] splitString = strMatrix.split("\\}\\s?,\\s?\\{");
+        for (int i = 0; i < splitString.length; i++) {
+            splitString[i] = splitString[i].replace("{", "").replace("}", "");
+        }
+        double[][] matrix = new double[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                String[] row = splitString[i].split(",");
                 matrix[i][j] = Double.parseDouble(row[j]);
             }
             this.value = matrix;
         }
     }
-
 
     @Override
     public String toString() {

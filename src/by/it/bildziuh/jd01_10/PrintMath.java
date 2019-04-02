5e4710a7945faba1166535d2f4da4eb0b110f792
Bildziuh <crazydaem@gmail.com>
2019-04-03 00:48:12 +0300

    jd01_10 - B1 через реплейс сделано. Если через гет, то еще надо найти тип значения передаваемого в метод.

--- src/by/it/bildziuh/jd01_10/PrintMath.java
+++ src/by/it/bildziuh/jd01_10/PrintMath.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
+
 public class PrintMath {
     public static void main(String[] args) {
 
@@ -12,12 +13,38 @@ public class PrintMath {
         Field[] fields = mathClass.getFields();
 
         for (Method method : methods) {
-            if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC)
-                System.out.println(method);
+            if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC) {
+                //System.out.println((String.valueOf(method).replace("java.lang.Math.", "")));
+                System.out.print(modifiers(method.getModifiers()));
+                System.out.print(method.getName());
+
+                if (method.getParameterCount() == 2)
+                    System.out.println("(" + method.getReturnType() + "," + method.getReturnType() + ")");
+                else
+                    System.out.println("(" + method.getReturnType() + ")");
+            }
         }
+
         for (Field field : fields) {
             if ((field.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC)
-                System.out.println(field);
+            System.out.println((String.valueOf(field).replace("java.lang.Math.", "")));
         }
     }
+
+    private static String modifiers(int mods) {
+        switch (mods) {
+            case 1:
+                return "public ";
+            case 9:
+                return "public static ";
+            case 17:
+                return "public final ";
+            case 257:
+                return "public native ";
+            case 273:
+                return "public final native ";
+        }
+        return null;
+    }
+
 }

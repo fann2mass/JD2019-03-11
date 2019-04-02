3a2ce20dfeac75b128c5c8923f13ec9f8257df97
Bildziuh <crazydaem@gmail.com>
2019-04-02 16:52:49 +0300

    jd01_10 - С1 done, B1 - удалить полный путь к методам.

--- src/by/it/bildziuh/jd01_10/PrintMath.java
+++ src/by/it/bildziuh/jd01_10/PrintMath.java
@@ -6,9 +6,11 @@ import java.lang.reflect.Modifier;
 
 public class PrintMath {
     public static void main(String[] args) {
+
         Class<Math> mathClass = Math.class;
         Method[] methods = mathClass.getMethods();
         Field[] fields = mathClass.getFields();
+
         for (Method method : methods) {
             if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC)
                 System.out.println(method);

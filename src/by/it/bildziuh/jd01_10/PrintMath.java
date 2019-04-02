5281fb0f4afa038de6f53b266515659fe0d2e2cd
Bildziuh <crazydaem@gmail.com>
2019-04-03 01:53:08 +0300

    jd01_10 - B1 через реплейс сделано. Если через гет, то еще надо найти тип значения передаваемого в метод.

--- src/by/it/bildziuh/jd01_10/PrintMath.java
+++ src/by/it/bildziuh/jd01_10/PrintMath.java
@@ -14,13 +14,18 @@ public class PrintMath {
 
         for (Method method : methods) {
             if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC) {
-//                System.out.println((String.valueOf(method).replace("java.lang.Math.", "")));
-
+                System.out.println((String.valueOf(method).replace("java.lang.Math.", "")));
+/*
                 int count = method.getParameterCount();
                 System.out.print(modifiers(method.getModifiers()));
                 System.out.print(method.getName());
                 System.out.println(parameters(count, method));
 
+                if (method.getTypeParameters()!=null)
+                    System.out.print(method.getParameters()[0].toString().replace("arg0", ""));
+                else
+                    System.out.print("void ");
+                */
             }
         }
 
@@ -29,7 +34,7 @@ public class PrintMath {
                 System.out.println((String.valueOf(field).replace("java.lang.Math.", "")));
         }
     }
-
+/*
     private static String modifiers(int mods) {
         switch (mods) {
             case 1:
@@ -49,12 +54,15 @@ public class PrintMath {
 
     private static String parameters(int count, Method method) {
         switch (count) {
+            case 1:
+                return "(" + method.getParameterTypes()[0] + ")";
             case 2:
                 return "(" + method.getParameterTypes()[0] + "," + method.getParameterTypes()[1] + ")";
             case 3:
                 return "(" + method.getParameterTypes()[0] + "," + method.getParameterTypes()[1] + "," + method.getParameterTypes()[2] + ")";
             default:
-                return "(" + method.getParameterTypes()[0] + ")";
+                return "()";
         }
     }
+    */
 }

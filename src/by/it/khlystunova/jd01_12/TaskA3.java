ad15d3616153f82520a47d2fad7cd675d19dc371
Khlystunova-Liza <lisa.epee@gmail.com>
2019-04-06 08:32:20 +0300

    jd01_12 Task A1 A2 A3 done

--- src/by/it/khlystunova/jd01_12/TaskA3.java
+++ src/by/it/khlystunova/jd01_12/TaskA3.java
@@ -1,4 +1,29 @@
 package by.it.khlystunova.jd01_12;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class TaskA3 {
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        int post = 0;
+        while (true){
+            String n = scanner.next();
+            if(n.equals("end"))break;
+            Integer next = Integer.parseInt(n);
+            if(next<0)
+            list.add(next);
+            if(next==0)
+                list.add(post,next);
+            if(next>0)
+                list.add(post++,next);
+
+        }
+        System.out.println(list);
+
+    }
+
 }

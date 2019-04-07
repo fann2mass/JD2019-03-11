adc3e4ad25b53b6904d35ee2a1d103d5823d0410
Konstantsin Bolotko <pro100kosti@gmail.com>
2019-04-07 15:13:49 +0300

    jd01_01 (all) jd01_02 (A,B) jd01_03 (all) jd01_04 (A,B) jd01_05 (all) jd01_06(A,B) jd01_07(all) jd01_08(all) jd01_09(all) jd01_10(A,B1) jd01_11(A,B (calc(A))) jd01_12(A,B)

--- src/by/it/bolotko/jd01_12/TaskB3.java
+++ src/by/it/bolotko/jd01_12/TaskB3.java
@@ -2,16 +2,16 @@ package by.it.bolotko.jd01_12;
 
 import java.util.*;
 
-public class TaskB2 {
+public class TaskB3 {
 
     public static void main(String[] args) {
-        Scanner quantity = new Scanner(System.in);
-        System.out.print("Quantity people: ");
-        int n = quantity.nextInt();
+        int n = 4096;
+        System.out.print("Quantity people: " + n);
         String[] name = new String[n];
         for (int i = 0; i < n; i++) {
-            System.out.print((i + 1) + " person's name: ");
-            name[i] = quantity.next();
+            name[i] = String.valueOf(((Math.random() * 10) + 1));
+            System.out.print((i + 1) + " person's name: " + name[i]);
+            System.out.println();
         }
         List<String> base = Arrays.asList(name);
         ArrayList<String> arra = new ArrayList<>(base);
@@ -51,24 +51,4 @@ public class TaskB2 {
         }
         return peoples.get(0);
     }
-
-
-    //static String process(ArrayList<String> peoples) {
-    //int count = 0;
-    //Iterator<String> it = peoples.iterator();
-    //while (peoples.size() > 1) {
-    //if (it.hasNext()) {
-    // it.next();
-    //count++;
-    //if (count == 2) {
-    // it.remove();
-    // count = 0;
-    // System.out.println(peoples);
-    //}
-    // } else {
-    // it = peoples.iterator();
-    // }
-    //  }
-    //    return peoples.toString();
-    // }
 }

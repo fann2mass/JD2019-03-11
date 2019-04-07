e8d6950379e63ff7cd144e55ddc49c4e6974e88c
yourGitHubusername <Arol_33@mail.ru  git config --global user.name Katyaaaa>
2019-04-07 19:36:26 +0300

    11. A B C 12. A B

--- src/by/it/eslaikouskaya/jd01_11/SetC.java
+++ src/by/it/eslaikouskaya/jd01_11/SetC.java
@@ -1,89 +1,11 @@
 package by.it.eslaikouskaya.jd01_11;
 
 import java.util.*;
+@SuppressWarnings ("all") public class SetC<E> implements Set<E> {
 
-public class SetC<T> implements List<T> {
+	private E[] elements = (E[]) new Object[0];
 
-	private T[]elements = (T[]) new Object[]{};
-	private int size = 0;
-
-	@Override
-	public boolean add(T t) {
-		if (size==elements.length)
-			elements= Arrays.copyOf(elements, (size*3)/2+1);
-		elements[size++]=t;
-		return true;
-	}
-
-
-	@Override
-	public void add(int index, T element) {
-		if (size==elements.length)
-			elements= Arrays.copyOf(elements, (size*3)/2+1);
-		System.arraycopy(elements, index, elements, index+1, size-index);
-		elements[index]=element;
-		size++;
-	}
-
-	@Override
-	public T remove(int index) {
-		T del = elements[index];
-		System.arraycopy(elements,index+1, elements,index,size-1-index);
-		size--;
-		return del;
-	}
-
-	@Override
-	public boolean remove(Object o) {
-		int index=indexOf(o);
-		if (index>-1) remove(index);
-		return (index>-1);
-	}
-
-
-	@Override
-	public T get(int index) {
-		return elements[index];
-	}
-
-
-
-	@Override
-	public T set(int index, T element) {
-		T buffer = elements[index];
-		elements[index]=element;
-		return buffer;
-	}
-
-
-
-	@Override
-	public int indexOf(Object o) {
-		if (o == null) {
-			for (int i = 0; i < size; i++)
-				if (elements[i] == null)
-					return i;
-		} else {
-			for (int i = 0; i < size; i++)
-				if (o.equals(elements[i]))
-					return i;
-		}
-		return -1;
-	}
-
-
-	@Override
-	public String toString() {
-		StringBuilder sb=new StringBuilder("[");
-		String delimiter="";
-		for (int i = 0; i < size; i++) {
-			sb.append(delimiter).append(elements[i]);
-			delimiter=", ";
-		}
-		sb.append("]");
-		return  sb.toString();
-	}
-
+	public int size = 0;
 
 	@Override
 	public int size() {
@@ -96,61 +18,108 @@ public class SetC<T> implements List<T> {
 	}
 
 	@Override
-	public boolean contains(Object o) {
-		for (int i = 0; i < size; i++)
-				if (o.equals(elements[i]))
+	public boolean add(E e) {
+		if(this.contains(e))
+			return false;
+		if (size==elements.length)
+			elements= Arrays.copyOf(elements, (size*3)/2+1);
+		elements[size++]=e;
 		return true;
-				return false;
 	}
 
-
-
-	//todo
 	@Override
-	public boolean containsAll(Collection<?> c) {
+	public boolean addAll(Collection<? extends E> c) {
+		E[] a = (E[]) c.toArray();
+		Collections.addAll(this, a);
+		return true;
+	}
+
+	public E remove(int index) {
+		E ret = elements[index];
+		System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+		size--;
+		return ret;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		int index = indexOf(o);
+		if (index >= 0) {
+			remove(index);
+			return true;
+		}
 		return false;
 	}
+	public int indexOf(Object o) {
+		if (o == null) {
+			for (int i = 0; i < size; i++) {
+				if (elements[i] == null)
+					return i;
+			}
+		} else
+			for (int i = 0; i < size; i++) {
+				if (o.equals(elements[i]))
+					return i;
+			}
 
-	//todo
-	@Override
-	public boolean removeAll(Collection<?> c){
-return false;
+		return -1;
 	}
 
 	@Override
-	public boolean addAll(Collection<? extends T> c) {
-		T[] a = (T[]) c.toArray();
-		int capacity = size + a.length;
-		if (capacity > elements.length)
-			elements = Arrays.copyOf(elements, elements.length * 3 / 2 + 1);
-		System.arraycopy(a,0, elements,size, a.length);
-		size += a.length;
-		return a.length != 0;
+	public boolean contains(Object o) {
+		int i1 = indexOf(o);
+		return i1 != -1;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		for (Object o : c) {
+			if(!this.contains(o))
+				return  false;
+		}
+		return  true;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		E[] a = (E[]) c.toArray();
+		for (E e : a) {
+			this.remove(e);
+		}
+		return  true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder("[");
+		for (int i = 0; i < size; i++) {
+			if (i > 0) sb.append(", ");
+			sb.append(elements[i]);
+		}
+		sb.append("]");
+		return sb.toString();
 	}
 
 
-
-
-
-
-	//stub
+	//stabs
 	@Override
-	public Iterator<T> iterator() {
+	public Spliterator<E> spliterator() {
+		return null;
+	}
+
+
+	@Override
+	public Iterator<E> iterator() {
 		return null;
 	}
 
 	@Override
 	public Object[] toArray() {
-		return new Object[0];
+		return null;
 	}
 
 	@Override
-	public boolean addAll(int index, Collection<? extends T> c) {
-		return false;
-	}
-
-	@Override
-	public <T1> T1[] toArray(T1[] a) {
+	public <E> E[] toArray(E[] a) {
 		return null;
 	}
 
@@ -161,25 +130,7 @@ return false;
 
 	@Override
 	public void clear() {
-
-	}
-	@Override
-	public int lastIndexOf(Object o) {
-		return 0;
-	}
-
-	@Override
-	public ListIterator<T> listIterator() {
-		return null;
-	}
-
-	@Override
-	public ListIterator<T> listIterator(int index) {
-		return null;
-	}
-
-	@Override
-	public List<T> subList(int fromIndex, int toIndex) {
-		return null;
+		size=0;
+		elements = Arrays.copyOf(elements, 0);
 	}
 }

526364a991bf62b6f0e4815e15202f47f3d2c5c0
Bildziuh <crazydaem@gmail.com>
2019-04-09 09:59:56 +0300

    jd01_11 логика

--- src/by/it/bildziuh/jd01_12/TaskB1.java
+++ src/by/it/bildziuh/jd01_12/TaskB1.java
@@ -1,7 +1,28 @@
 package by.it.bildziuh.jd01_12;
 
+import java.util.*;
+
 public class TaskB1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, Integer> words = new LinkedHashMap<String, Integer>();
+        String word;
+        while (!(word = scanner.next()).equals("end")) {
+            if (words.containsKey(word)) {
+                int newValue = words.get(word);
+                words.replace(word, ++newValue);
+            } else {
+                words.put(word, 1);
+            }
+        }
+        print(words);
+    }
 
 
+    private static void print(Map<String,Integer> list) {
+        for (Map.Entry<String, Integer> entry : list.entrySet())
+            System.out.println(entry.getKey() + " = " + entry.getValue());
+    }
 
 }
+

c0a67323580b4327d78d6735a144db7538e6f1b8
Bildziuh <crazydaem@gmail.com>
2019-04-09 10:31:36 +0300

    jd01_12

--- src/by/it/bildziuh/jd01_12/TaskB1.java
+++ src/by/it/bildziuh/jd01_12/TaskB1.java
@@ -3,21 +3,31 @@ package by.it.bildziuh.jd01_12;
 import java.util.*;
 
 public class TaskB1 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Map<String, Integer> words = new LinkedHashMap<String, Integer>();
-        String word;
-        while (!(word = scanner.next()).equals("end")) {
-            if (words.containsKey(word)) {
-                int newValue = words.get(word);
-                words.replace(word, ++newValue);
-            } else {
-                words.put(word, 1);
-            }
+        Map<String, Integer> list = new HashMap<>();
+        String line;
+        String [] words;
+
+        while (!(line = scanner.nextLine()).equals("end")) {
+            line = line.replace("[—,\";.]", " ");
+            words = line.split(" ");
+            logic (words,list);
         }
-        print(words);
+        print(list);
     }
 
+
+    private static void logic (String[] words,Map<String,Integer> list){
+        for (String word : words) {
+            if (list.containsKey(word)) {
+                int newValue = list.get(word);
+                list.replace(word, ++newValue);
+            } else
+                list.put(word, 1);
+        }
+    }
 
     private static void print(Map<String,Integer> list) {
         for (Map.Entry<String, Integer> entry : list.entrySet())

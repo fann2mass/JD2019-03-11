4572390f4be15361ff4f8b344e38054714edb617
Bildziuh <crazydaem@gmail.com>
2019-04-09 12:33:35 +0300

    jd01_13 B done

---
+++ src/by/it/bildziuh/jd01_13/TaskB.java
@@ -0,0 +1,40 @@
+package by.it.bildziuh.jd01_13;
+
+
+import java.util.Scanner;
+
+public class TaskB {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String line;
+        double number = 0;
+        double result;
+
+        try {
+            while (!(line = scanner.nextLine()).equals("END")) {
+
+                number += Double.parseDouble(line);
+                System.out.println(number);
+
+                result = Math.sqrt(number);
+                if (number < 0)
+                    throw new ArithmeticException();
+                System.out.println(result);
+            }
+
+        } catch (NumberFormatException | ArithmeticException e) {
+
+            StackTraceElement[] stackTrace = e.getStackTrace();
+            for (StackTraceElement element : stackTrace) {
+                if (TaskB.class.getName().equals(element.getClassName())) {
+                    String name = e.getClass().getName();
+                    String className = element.getClassName();
+                    int lineNum = element.getLineNumber();
+                    System.out.printf("name:%s\nclass:%s\nline:%s\n", name, className, lineNum);
+                }
+
+            }
+        }
+    }
+}

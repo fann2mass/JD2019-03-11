d2fd6a545a4db9f650622e3e82f72fc7f5028397
Konstantsin Bolotko <pro100kosti@gmail.com>
2019-04-12 09:23:00 +0300

    jd01_01 (all) jd01_02 (A,B) jd01_03 (all) jd01_04 (A,B) jd01_05 (all) jd01_06(A,B) jd01_07(all) jd01_08(all) jd01_09(all) jd01_10(all) jd01_11(A,B (calc(A))) jd01_12(A,B) jd01_13(A,B2,C2) jd01_14(A)

--- src/by/it/bolotko/jd01_10/Bean.java
+++ src/by/it/bolotko/jd01_10/Bean.java
@@ -2,22 +2,27 @@ package by.it.bolotko.jd01_10;
 
 public class Bean {
 
-    static double sum(int a, int b) {
-        return a + b;
+
+    double sum(int a, int b){
+        return a+b;
     }
 
-    @Param(a = 8, b = 10)
-    static double min(int a, int b) {
-        return a < b ? a : b;
+    @Param(a = 2, b = 5)
+    static double max(int a, int b){
+        if (a==b) System.out.println("a=b");
+        else if (a>b) return a;
+        return b;
     }
 
-    @Param(a = 8, b = 10)
-    double max(int a, int b) {
-        return a > b ? a : b;
+    @Param(a = 2, b = 5)
+    static double min(int a, int b){
+        if (a==b) System.out.println("a=b");
+        else if (a>b) return b;
+        return a;
     }
 
-    @Param(a = 8, b = 10)
-    double avg(int a, int b) {
-        return (a + b) / 2.0;
+    @Param(a = 2, b = 5)
+    double avg(int a, int b){
+        return (a+b)/2.0;
     }
 }

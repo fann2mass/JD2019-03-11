5d582ea99ea002d807c5551a3c2f80bf6dc547ae
Bildziuh <crazydaem@gmail.com>
2019-04-13 01:09:12 +0300

    jd01_14 допилил рекурсию

---
+++ src/by/it/bildziuh/jd01_14/TaskC.java
@@ -0,0 +1,43 @@
+package by.it.bildziuh.jd01_14;
+
+import java.io.*;
+
+public class TaskC {
+
+    public static void main(String[] args) {
+        String path = getPath(TaskC.class);
+        String outTxt = path + File.separator + "resultTaskC.txt";
+        File f = new File(path);
+
+        CheckFolder(f.getParent(), outTxt);
+    }
+
+    private static void CheckFolder(String dirPath, String outTxt) {
+        try (PrintWriter printWriter = new PrintWriter(new FileWriter(outTxt))) {
+
+            File file = new File(dirPath);
+            File[] allFiles = file.listFiles();
+
+            System.out.println("dir:" + file.getName());
+            printWriter.println("dir:" + file.getName());
+
+            for (File currentFile : allFiles) {
+                if (currentFile.isDirectory()) {
+                    CheckFolder(currentFile.getAbsolutePath(), outTxt);
+                } else {
+                    printWriter.println("\tfile:" + currentFile.getName());
+                    System.out.println("\tfile:" + currentFile.getName());
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getPath(Class<?> cl) {
+        String userDir = System.getProperty("user.dir") + File.separator + "src" + File.separator;
+        String pathPack = cl.getPackage().getName().replace(".", File.separator);
+        return userDir + pathPack;
+    }
+}

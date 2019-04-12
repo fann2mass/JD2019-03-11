d8b650cd8a10fb0e0ccac889151ec4e7907e76ff
Bildziuh <crazydaem@gmail.com>
2019-04-13 01:33:43 +0300

    jd01_14 допилил рекурсию

--- src/by/it/bildziuh/jd01_14/TaskC.java
+++ src/by/it/bildziuh/jd01_14/TaskC.java
@@ -6,32 +6,29 @@ public class TaskC {
 
     public static void main(String[] args) {
         String path = getPath(TaskC.class);
-        String outTxt = path + File.separator + "resultTaskC.txt";
         File f = new File(path);
 
-        CheckFolder(f.getParent(), outTxt);
-    }
-
-    private static void CheckFolder(String dirPath, String outTxt) {
-        try (PrintWriter printWriter = new PrintWriter(new FileWriter(outTxt))) {
-
-            File file = new File(dirPath);
-            File[] allFiles = file.listFiles();
-
-            System.out.println("dir:" + file.getName());
-            printWriter.println("dir:" + file.getName());
-
-            for (File currentFile : allFiles) {
-                if (currentFile.isDirectory()) {
-                    CheckFolder(currentFile.getAbsolutePath(), outTxt); // вот она, родная — рекурсия!
-                } else {
-                    printWriter.println("\tfile:" + currentFile.getName());
-                    System.out.println("\tfile:" + currentFile.getName());
-                }
-            }
-
+        try (PrintWriter printWriter = new PrintWriter(new FileWriter(path + File.separator + "resultTaskC.txt"))) {
+            CheckFolder(f.getParent(), printWriter);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+    private static void CheckFolder(String dirPath, PrintWriter printWriter) {
+
+        File file = new File(dirPath);
+        File[] allFiles = file.listFiles();
+
+        System.out.println("dir:" + file.getName());
+        printWriter.println("dir:" + file.getName());
+
+        for (File currentFile : allFiles) {
+            if (currentFile.isDirectory())
+                CheckFolder(currentFile.getAbsolutePath(), printWriter); // вот она, родная — рекурсия!
+            else {
+                printWriter.println("\tfile:" + currentFile.getName());
+                System.out.println("\tfile:" + currentFile.getName());
+            }
         }
     }
 
@@ -39,6 +36,5 @@ public class TaskC {
         String path = System.getProperty("user.dir") + File.separator + "src" + File.separator;
         String classDir = cl.getName().replace(cl.getSimpleName(), "").replace(".", File.separator);
         return path + classDir;
-
     }
 }

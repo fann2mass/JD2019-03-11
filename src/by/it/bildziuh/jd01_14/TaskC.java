287ca709daf400aa0e185524c5a82c2347f1d139
Bildziuh <crazydaem@gmail.com>
2019-04-13 10:40:17 +0300

    jd01_14 попытки прикрутить счётчик

--- src/by/it/bildziuh/jd01_14/TaskC.java
+++ src/by/it/bildziuh/jd01_14/TaskC.java
@@ -3,8 +3,7 @@ package by.it.bildziuh.jd01_14;
 import java.io.*;
 
 public class TaskC {
-    private static int counter = 1;
-
+   // private static int counter = 1;
     public static void main(String[] args) {
         String path = getPath(TaskC.class);
         File f = new File(path);
@@ -17,23 +16,21 @@ public class TaskC {
     }
 
     private static void CheckFolder(String dirPath, PrintWriter printWriter) {
-        String offSet = String.format("%" + counter + "s", " ");
-
+      //  String offSet = String.format("%" + counter + "s", " ");
         File file = new File(dirPath);
         File[] allFiles = file.listFiles();
 
-        System.out.println(offSet + "dir:" + file.getName());
-        printWriter.println(offSet + "dir:" + file.getName());
-        counter++;
+        System.out.println("dir:" + file.getName());
+        printWriter.println("dir:" + file.getName());
+     //   counter++;
         for (File currentFile : allFiles) {
             if (currentFile.isDirectory()) {
                 CheckFolder(currentFile.getPath(), printWriter); // вот она, родная — рекурсия!
             } else {
-                printWriter.println(offSet + "file:" + currentFile.getName());
-                System.out.println(offSet + "file:" + currentFile.getName());
+                printWriter.println("\tfile:" + currentFile.getName());
+                System.out.println("\tfile:" + currentFile.getName());
             }
         }
-        counter--;
     }
 
     private static String getPath(Class<?> cl) {

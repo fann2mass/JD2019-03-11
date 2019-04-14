132ca5a353eb91028c866c3205ae767717322f7e
Bildziuh <crazydaem@gmail.com>
2019-04-14 18:07:15 +0300

    jd01_15 C done

--- src/by/it/bildziuh/jd01_15/TaskC.java
+++ src/by/it/bildziuh/jd01_15/TaskC.java
@@ -1,9 +1,6 @@
 package by.it.bildziuh.jd01_15;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class TaskC {
@@ -11,18 +8,23 @@ public class TaskC {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String line;
-        String path = getPath(TaskC.class);
 
         while (!(line = scanner.nextLine()).equals("end")) {
             if (line.contains("cd")) {
-                path = cd(path, line);
-            }
-            if (line.contains("dir")) {
+                cd(path, line);
+            } else if (line.contains("dir")) {
                 dir(path);
-            }
-            if (!(line.contains("cd") || line.contains("dir")))
+            } else
                 System.out.println("Неверная команда!");
         }
+    }
+
+    private static String path = getPath(TaskC.class);
+
+    private static String getPath(Class<?> cl) {
+        String path = System.getProperty("user.dir") + File.separator + "src" + File.separator;
+        String classDir = cl.getName().replace(cl.getSimpleName(), "").replace(".", File.separator);
+        return path + classDir;
     }
 
     private static void dir(String dirPath) {
@@ -34,32 +36,25 @@ public class TaskC {
         }
     }
 
-    private static String getPath(Class<?> cl) {
-        String path = System.getProperty("user.dir") + File.separator + "src" + File.separator;
-        String classDir = cl.getName().replace(cl.getSimpleName(), "").replace(".", File.separator);
-        return path + classDir;
-    }
-
-    private static String cd(String oldPath, String command) {
-        StringBuilder newFolder = new StringBuilder(command);
-        newFolder.delete(0, 3);
+    private static void cd(String oldPath, String command) {
 
         StringBuilder newPath = new StringBuilder(oldPath);
 
         if (command.contains("..")) {
             newPath.delete(newPath.lastIndexOf("\\"), newPath.length()).delete(newPath.lastIndexOf("\\") + 1, newPath.length());
-            return newPath.toString();
+            path = newPath.toString();
         }
+
+        StringBuilder newFolder = new StringBuilder(command);
+        newFolder.delete(0, 3);
 
         for (File currentFile : getFile(oldPath).listFiles()) {
             if (newFolder.toString().equals(currentFile.getName())) {
                 newPath.append(currentFile.getName()).append(File.separator);
-                return newPath.toString();
+                path = newPath.toString();
             }
         }
-
         System.out.println("Неверно указан путь!");
-        return newPath.toString();
     }
 
     private static File getFile(String path) {

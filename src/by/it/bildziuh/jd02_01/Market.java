7bd57c01952286b7f4b7d5b923273b4370cda144
Bildziuh <crazydaem@gmail.com>
2019-04-18 23:07:22 +0300

    jd02_01 допилить C

--- src/by/it/bildziuh/jd02_01/Market.java
+++ src/by/it/bildziuh/jd02_01/Market.java
@@ -27,9 +27,8 @@ public class Market {
                 if (time <= 30 && Dispatcher.buyerInMarket <= time + 10)
                     continue;
 
-                Util.sleep(1000);
+                Util.sleep(100);
                 time++;
-
             }
         }
         for (Thread th : buyers) {

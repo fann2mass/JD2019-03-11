87992775027c4ea61d173364102bdb11a1215dd9
Narushevich Marianna <marianna.naru@gmail.com>
2019-04-22 11:23:34 +0300

    jd02_03 ref

--- src/by/it/narushevich/jd02_03/QueueBuyers.java
+++ src/by/it/narushevich/jd02_03/QueueBuyers.java
@@ -1,5 +1,6 @@
 package by.it.narushevich.jd02_03;
 
+import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 
@@ -8,7 +9,7 @@ class QueueBuyers {
     private QueueBuyers(){
     }
 
-    private static final BlockingQueue<Buyer> BUYERS = new LinkedBlockingDeque<>(30);
+    private static final BlockingDeque<Buyer> BUYERS = new LinkedBlockingDeque<>(30);
 
     public static BlockingQueue<Buyer> getBUYERS() {
         return BUYERS;
@@ -16,6 +17,7 @@ class QueueBuyers {
 
     static synchronized void add(Buyer buyer) {
         BUYERS.add(buyer);
+        if(buyer.pensioneer) BUYERS.addFirst(buyer);
 
     }
     static synchronized Buyer extract() {

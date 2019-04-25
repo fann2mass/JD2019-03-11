e0a735725d21639c247da39c0af95fb336f9b7c2
yourGitHubusername <Arol_33@mail.ru  git config --global user.name Katyaaaa>
2019-04-25 10:26:26 +0300

    02.05

--- src/by/it/eslaikouskaya/jd02_04/Patterns.java
+++ src/by/it/eslaikouskaya/jd02_04/Patterns.java
@@ -5,7 +5,7 @@ class Patterns {
 	private Patterns() {
 	}
 
-	static final String BRACKETS="[(]([A-Za-z]|(-?\\d+(\\.\\d+)?))(([-+*/=])([A-Za-z]|(-?\\d+(\\.\\d+)?)))+[)]]";
+	static final String BRACKETS="[(]([A-Za-z]|(-?\\d+(\\.\\d+)?))([-+*/=])([A-Za-z]|(-?\\d+(\\.\\d+)?))?[)]";
 	static final String OPERATION="(?<=[^-+*=/,{])([-+*/=])";
 	static final String SCALAR="-?\\d+(\\.\\d+)?";
 	static final String VECTOR="[{]((-?\\d+(\\.\\d+)?),?\\s?)+[}]";

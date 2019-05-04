e7e20c8e662e16bc1167360d6bc677d1dfd4a047
Bolotko Konstantsin <pro100kosti@gmail.com>
2019-05-04 17:29:34 +0300

    jd01_15(A,B) jd02_07(all) jd02_08(begin)

---
+++ src/by/it/bolotko/jd02_08/SaxHandler.java
@@ -0,0 +1,6 @@
+package by.it.bolotko.jd02_08;
+
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SaxHandler extends DefaultHandler {
+}
---
+++ src/by/it/bolotko/jd02_08/SaxParser.java
@@ -0,0 +1,10 @@
+package by.it.bolotko.jd02_08;
+
+public class SaxParser {
+
+    private static String xml="D:\\bolotko\\JD2019-03-11\\src\\by\\it\\bolotko\\jd02_07\\parkings+xsd.xml";
+
+    public static void main(String[] args) {
+
+    }
+}

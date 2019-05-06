4d853d2bfe8c3a061a01e09666d75db841700052
Narushevich Marianna <marianna.naru@gmail.com>
2019-05-06 11:36:15 +0300

    jd02_08 class

---
+++ src/by/it/narushevich/jd02_08/Path.java
@@ -0,0 +1,8 @@
+package by.it.narushevich.jd02_08;
+
+class Path {
+    private Path(){}
+
+    static final String XML = "D:/Marianna/JD2019-03-20/src/by/it/narushevich/jd02_07/users+xsd.xml";
+
+}
---
+++ src/by/it/narushevich/jd02_08/SaxHandler.java
@@ -0,0 +1,37 @@
+package by.it.narushevich.jd02_08;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SaxHandler extends DefaultHandler {
+
+    private String tab;
+    private StringBuilder text;
+
+    @Override
+    public void startDocument() {
+        System.out.println("Start Document");
+    }
+
+    @Override
+    public void endDocument() {
+        System.out.println("End Document");
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        tab = "";
+        super.startElement(uri, localName, qName, attributes);
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        super.endElement(uri, localName, qName);
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        super.characters(ch, start, length);
+    }
+}

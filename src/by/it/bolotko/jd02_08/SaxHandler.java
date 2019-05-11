6fe4c74327379179c508b77d0e113f2932a58481
Bolotko Konstantsin <pro100kosti@gmail.com>
2019-05-11 14:21:40 +0300

    jd01_15(A,B) jd02_07(all) jd02_08(A)

--- src/by/it/bolotko/jd02_08/SaxHandler.java
+++ src/by/it/bolotko/jd02_08/SaxHandler.java
@@ -1,6 +1,26 @@
 package by.it.bolotko.jd02_08;
 
-import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.SAXException;
 
-public class SaxHandler extends DefaultHandler {
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.IOException;
+
+public class SaxHandler {
+
+    private static String xml = "src/by/it/bolotko/jd02_07/parkings.xml";
+
+    public static void main(String[] args) {
+
+        try {
+            SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+            SAXParser saxParser = saxParserFactory.newSAXParser();
+            SaxParser handler = new SaxParser();
+            saxParser.parse(new File(xml), handler);
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 }

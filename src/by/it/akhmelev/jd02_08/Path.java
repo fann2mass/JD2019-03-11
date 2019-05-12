3d2bb44ed16e3f0300c7af834b19653577811984
Александр Хмелев <Khmelov@users.noreply.github.com>
2019-05-12 23:35:26 +0300

    Update Path.java

--- src/by/it/akhmelev/jd02_08/Path.java
+++ src/by/it/akhmelev/jd02_08/Path.java
@@ -4,8 +4,11 @@ class Path {
     private Path() {
     }
 
-	static final String XML = "/Users/ekaterina/Documents/projects/src/by/it/eslaikouskaya/JD2019-03-11/src/by/it/akhmelev/jd02_07/users.xml";
-	static final String XSL = "/Users/ekaterina/Documents/projects/src/by/it/eslaikouskaya/JD2019-03-11/src/by/it/akhmelev/jd02_08/rules.xsl";
-	static final String HTML = "/Users/ekaterina/Documents/projects/src/by/it/eslaikouskaya/JD2019-03-11/src/by/it/akhmelev/jd02_08/user.html";
+    static final String XML=System.getProperty("user.dir")+
+            "/src/by/it/akhmelev/jd02_07/users.xml";
+    static final String XSL =System.getProperty("user.dir")+
+            "/src/by/it/akhmelev/jd02_08/rules.xsl";
+    static final String HTML=System.getProperty("user.dir")+
+            "/src/by/it/akhmelev/jd02_08/user.html";
 
 }

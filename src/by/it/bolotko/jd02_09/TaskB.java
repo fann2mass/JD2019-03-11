689ac66d501a2fae82c74d1e52238156db01f4db
Bolotko Konstantsin <pro100kosti@gmail.com>
2019-05-14 17:10:36 +0300

    jd01_15(A,B) jd02_07(all) jd02_08(A,B) jd02_09(A,B)

--- src/by/it/bolotko/jd02_09/TaskB.java
+++ src/by/it/bolotko/jd02_09/TaskB.java
@@ -12,64 +12,49 @@ import java.io.IOException;
 public class TaskB {
 
     public static void main(String[] args) throws JAXBException, IOException {
-        readXML();
-        saveXML();
-        readJson(getJson());
-   }
-
-        private static void readXML() throws JAXBException {
-            Parkings parkings;
-            JAXBContext jaxbContext = JAXBContext.newInstance(Parkings.class);
-            String FromXml = "src/by/it/bolotko/jd02_09/parkings+xsd.xml";
-            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-            try (FileInputStream fileInputStream = new FileInputStream(FromXml)) {
-                parkings = (Parkings) unmarshaller.unmarshal(fileInputStream);
-                System.out.println("==================== PARKINGS FROM XML ==============================");
-                System.out.println(parkings);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+        JAXBContext jaxbContext = JAXBContext.newInstance(Parkings.class);
+        Parkings parkings;
+        String FromXml = "src/by/it/bolotko/jd02_09/parkings+xsd.xml";
+        String ToXml = "src/by/it/bolotko/jd02_09/resultB.xml";
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        try (FileInputStream fileInputStream = new FileInputStream(FromXml)) {
+            parkings = (Parkings) unmarshaller.unmarshal(fileInputStream);
         }
+        Parkings PFX = parkings;
 
-        private static void saveXML() throws JAXBException, IOException {
-            JAXBContext jaxbContext = JAXBContext.newInstance(Parkings.class);
-            Parkings parkings;
-            String FromXml = "src/by/it/bolotko/jd02_09/parkings+xsd.xml";
-            String ToXml = "src/by/it/bolotko/jd02_09/resultB.xml";
-            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-            try (FileInputStream fileInputStream = new FileInputStream(FromXml)) {
-                parkings = (Parkings) unmarshaller.unmarshal(fileInputStream);
-            }
-            Marshaller marshaller = jaxbContext.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-            try (FileOutputStream fileOutputStreamStream = new FileOutputStream(ToXml)) {
-                marshaller.marshal(parkings, fileOutputStreamStream);
-                System.out.println("==================== XML FROM PARKINGS ==============================");
-                marshaller.marshal(parkings, System.out);
-            }
-        }
+        readXML(PFX);
+        saveXML(jaxbContext, ToXml, PFX);
+        readJson(getJson(PFX));
+    }
 
-        private static String getJson() throws JAXBException, IOException {
-            JAXBContext jaxbContext = JAXBContext.newInstance(Parkings.class);
-            String FromXml = "src/by/it/bolotko/jd02_09/parkings+xsd.xml";
-            Parkings parkings;
-            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-            try (FileInputStream fileInputStream = new FileInputStream(FromXml)) {
-                parkings = (Parkings) unmarshaller.unmarshal(fileInputStream);
-            }
+    private static void readXML(Parkings PFX) {
+        System.out.println("==================== PARKINGS FROM XML ==============================");
+        System.out.println(PFX);
+    }
 
-            Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
-            String json = gson.toJson(parkings);
-            System.out.println("==================== JSON FROM PARKINGS ==============================");
-            System.out.println(json);
-            return json;
-        }
-
-        private static void readJson(String json){
-            Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
-            Parkings parkings2 = gson.fromJson(json, Parkings.class);
-            System.out.println("==================== PARKINGS FROM JSON  ==============================");
-            System.out.println(parkings2.toString());
+    private static void saveXML(JAXBContext jaxbContext, String ToXml, Parkings PFX) throws JAXBException, IOException {
+        Marshaller marshaller = jaxbContext.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        try (FileOutputStream fileOutputStreamStream = new FileOutputStream(ToXml)) {
+            marshaller.marshal(PFX, fileOutputStreamStream);
+            System.out.println("\n==================== XML FROM PARKINGS ==============================");
+            marshaller.marshal(PFX, System.out);
         }
     }
+
+    private static String getJson(Parkings PFX) {
+        Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
+        String json = gson.toJson(PFX);
+        System.out.println("\n==================== JSON FROM PARKINGS ==============================");
+        System.out.println(json);
+        return json;
+    }
+
+    private static void readJson(String json) {
+        Gson gson = new GsonBuilder().serializeNulls().setPrettyPrinting().create();
+        Parkings PFJ = gson.fromJson(json, Parkings.class);
+        System.out.println("\n==================== PARKINGS FROM JSON  ==============================");
+        System.out.println(PFJ.toString());
+    }
+}
 

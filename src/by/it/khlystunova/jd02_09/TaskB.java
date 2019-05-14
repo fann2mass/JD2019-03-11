611cef5f74048aad4edce67d38875cd435c0f6ae
Khlystunova-Liza <lisa.epee@gmail.com>
2019-05-14 10:49:57 +0300

    jd02_09 A B in process

--- src/by/it/khlystunova/jd02_09/TaskB.java
+++ src/by/it/khlystunova/jd02_09/TaskB.java
@@ -10,24 +10,38 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-public class TaskA {
+public class TaskB {
 
-    public static void main(String[] args) throws JAXBException,IOException {
+    static Clients clients;
 
+    public static void main(String[] args) throws JAXBException, IOException {
+
+        readXml();
+        saveXml();
+
+
+    }
+
+    static void readXml() throws JAXBException, IOException {
+        System.out.println("========readXML========");
         String FROM_XML ="src/by/it/khlystunova/jd02_09/clients+xsd.xml";//демаршализация
-        String TO_XML ="src/by/it/khlystunova/jd02_09/clients+xsd+jaxb.xml";
         JAXBContext jaxbContext = JAXBContext.newInstance(Clients.class);
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-        Clients clients;
         try(FileInputStream inputStream = new FileInputStream(FROM_XML)){
-            clients =(Clients) unmarshaller.unmarshal(inputStream);
+            TaskB.clients =(Clients) unmarshaller.unmarshal(inputStream);
             System.out.println(clients);
         }
 
+    }
+
+    static void saveXml() throws JAXBException, IOException {
+        System.out.println("========saveXML========");
+        String TO_XML ="src/by/it/khlystunova/jd02_09/resultB.xml";
+        JAXBContext jaxbContext = JAXBContext.newInstance(Clients.class);
         Marshaller marshaller = jaxbContext.createMarshaller();
         try(FileOutputStream out = new FileOutputStream(TO_XML)){
-            marshaller.marshal(clients,out);
-            marshaller.marshal(clients,System.out);
+            marshaller.marshal(TaskB.clients,out);
+            marshaller.marshal(TaskB.clients,System.out);
         }
     }
 }

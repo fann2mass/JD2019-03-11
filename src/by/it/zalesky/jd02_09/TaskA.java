e73b870e09190b9cccd6aa5b0b44005917390483
User <ifann2mass@gmail.com>
2019-05-15 18:48:37 +0300

    commit

--- src/by/it/zalesky/jd02_09/TaskA.java
+++ src/by/it/zalesky/jd02_09/TaskA.java
@@ -1,4 +1,12 @@
 package by.it.zalesky.jd02_09;
 
 public class TaskA {
+    public static void main(String[] args) {
+
+        String fromXml = "src/by/it/zalesky/jd02_09/customers+xsd.xml";
+        String toXml = "src/by/it/zaleskyjd02_09/customers+xsd+jaxb.xml";
+
+
+
+    }
 }

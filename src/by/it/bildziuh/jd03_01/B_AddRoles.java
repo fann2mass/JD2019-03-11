d60796405cb028867a0cc222c13042e8aeb48896
Bildziuh <crazydaem@gmail.com>
2019-05-16 19:54:52 +0300

    .

--- src/by/it/bildziuh/jd03_01/B_AddRoles.java
+++ src/by/it/bildziuh/jd03_01/B_AddRoles.java
@@ -16,11 +16,18 @@ public class B_AddRoles {
                 Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
                 Statement statement = connection.createStatement()
         ) {
+
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS `bildziuh`.`roles` (\n" +
+                    "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                    "  `role` VARCHAR(100) NULL,\n" +
+                    "  PRIMARY KEY (`ID`))\n" +
+                    "ENGINE = InnoDB;\n");
+
             statement.executeUpdate("INSERT INTO `bildziuh`.`roles` (`ID`, `Role`) VALUES (DEFAULT, 'Administrator');\n");
             statement.executeUpdate("INSERT INTO `bildziuh`.`roles` (`ID`, `Role`) VALUES (DEFAULT, 'Moderator');\n");
             statement.executeUpdate("INSERT INTO `bildziuh`.`roles` (`ID`, `Role`) VALUES (DEFAULT, 'User');\n");
             statement.executeUpdate("INSERT INTO `bildziuh`.`roles` (`ID`, `Role`) VALUES (DEFAULT, 'Guest');\n");
-            statement.executeUpdate("INSERT INTO `bildziuh`.`roles` (`ID`, `Role`) VALUES (DEFAULT, 'TestRole');\n");
+
         }
     }
 }

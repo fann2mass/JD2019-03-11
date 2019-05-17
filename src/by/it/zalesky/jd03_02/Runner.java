6c83bf64b8cb091664b3812ba9c6d5eaa55cc861
User <ifann2mass@gmail.com>
2019-05-17 19:41:35 +0300

    just commit

---
+++ src/by/it/zalesky/jd03_02/Runner.java
@@ -0,0 +1,8 @@
+package by.it.zalesky.jd03_02;
+
+public class Runner {
+    public static void main(String[] args) {
+
+//        User user = new User ()
+    }
+}
---
+++ src/by/it/zalesky/jd03_02/UserCRUD.java
@@ -0,0 +1,8 @@
+package by.it.zalesky.jd03_02;
+
+public class UserCRUD {
+
+
+    }
+
+

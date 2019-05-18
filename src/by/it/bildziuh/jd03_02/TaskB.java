2627f1e52d8b6ada7689bb3ba679a862994c6cbc
Bildziuh <crazydaem@gmail.com>
2019-05-18 17:20:08 +0300

    jd02_09

--- src/by/it/bildziuh/jd03_02/TaskB.java
+++ src/by/it/bildziuh/jd03_02/TaskB.java
@@ -1,6 +1,7 @@
 package by.it.bildziuh.jd03_02;
 
 
+import javax.xml.transform.Result;
 import java.sql.*;
 
 public class TaskB {
@@ -15,7 +16,8 @@ public class TaskB {
                 Statement statement = connection.createStatement()) {
 
             //получаем пользователей. Если неизвестно число полей, то нужно получить метаданные
-            int count = 0;
+            int countUsers = 0;
+            int countRoles = 0;
             ResultSet resultSet = statement.executeQuery("SELECT * FROM users INNER JOIN roles ON users.roles_ID=roles.ID");
             while (resultSet.next()) {
                 ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
@@ -26,10 +28,14 @@ public class TaskB {
                                     resultSet.getString(i) + "\t\t"
                     );
                 System.out.println();
-                count++;
+                countUsers++;
             }
-
-            System.out.println("Всего записей: " + count);
+            ResultSet result = statement.executeQuery("SELECT * FROM roles");
+            while (result.next()) {
+                countRoles++;
+            }
+            System.out.println("Всего пользователей: " + countUsers);
+            System.out.println("Всего возможных ролей: " + countRoles);
 
         } catch (Exception e) {
             e.printStackTrace();
